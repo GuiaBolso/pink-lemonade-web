2f2c0cbc4544d8ae81ab829fc3a5cbb69267a7f2
Gustavo Bento de Paula <gustavob.paula@gmail.com>
2020-03-24 16:23:36 -0300

    test: :white_check_mark: add Text tests

--- libs/ui/jest.config.js
+++ libs/ui/jest.config.js
@@ -6,4 +6,8 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../coverage/libs/ui',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+  },
 };
