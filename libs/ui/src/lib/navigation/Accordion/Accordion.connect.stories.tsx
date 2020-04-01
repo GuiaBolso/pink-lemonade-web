0e10bd2a39bb4b38626507120e2baac86c679ffb
Gustavo Bento de Paula <gustavob.paula@gmail.com>
2020-04-01 16:42:41 -0300

    feat: :art: add storybook

---
+++ libs/ui/src/lib/navigation/Accordion/Accordion.connect.stories.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { Theme } from '../../general';
+import { connect } from '../../../designSystem/tokens';
+import Accordion from './Accordion';
+
+export default {
+  title: 'Navigation/Accordion/Connect',
+};
+
+export const base = () => (
+  <Theme theme={connect}>
+    <Accordion summary={<span>Lorem Ipsum</span>}>
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+        commodo.
+      </span>
+    </Accordion>
+    <Accordion summary={<span>Lorem Ipsum</span>}>
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at
+        rhoncus eros, at ullamcorper nibh. In a cursus erat. Donec.
+      </span>
+    </Accordion>
+    <Accordion summary={<span>Lorem Ipsum</span>}>
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec
+        lorem vitae metus gravida cursus vitae fringilla odio. Vestibulum
+        interdum ac neque et vulputate. Aenean pellentesque velit condimentum,
+        consequat nunc ut, semper est. Morbi non mi varius, hendrerit ex sit
+        amet, maximus justo. Aenean pretium ligula id dolor bibendum tincidunt.
+      </span>
+    </Accordion>
+    <Accordion summary={<span>Lorem Ipsum</span>}>
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+        commodo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+        nec lorem vitae metus gravida cursus vitae fringilla odio. Vestibulum
+        interdum ac neque et vulputate. Aenean pellentesque velit condimentum,
+        consequat nunc ut, semper est. Morbi non mi varius, hendrerit ex sit
+        amet, maximus justo. Aenean pretium ligula id dolor bibendum tincidunt.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec
+        lorem vitae metus gravida cursus vitae fringilla odio. Vestibulum
+        interdum ac neque et vulputate. Aenean pellentesque velit condimentum,
+        consequat nunc ut, semper est. Morbi non mi varius, hendrerit ex sit
+        amet, maximus justo. Aenean pretium ligula id dolor bibendum tincidunt.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec
+        lorem vitae metus gravida cursus vitae fringilla odio. Vestibulum
+        interdum ac neque et vulputate. Aenean pellentesque velit condimentum,
+        consequat nunc ut, semper est. Morbi non mi varius, hendrerit ex sit
+        amet, maximus justo. Aenean pretium ligula id dolor bibendum tincidunt.
+      </span>
+    </Accordion>
+  </Theme>
+);
+
+export const expanded = () => (
+  <Theme theme={connect}>
+    <Accordion initOpened summary={<span>Lorem Ipsum</span>}>
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+        commodo.
+      </span>
+    </Accordion>
+    <Accordion initOpened summary={<span>Lorem Ipsum</span>}>
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at
+        rhoncus eros, at ullamcorper nibh. In a cursus erat. Donec.
+      </span>
+    </Accordion>
+    <Accordion initOpened summary={<span>Lorem Ipsum</span>}>
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec
+        lorem vitae metus gravida cursus vitae fringilla odio. Vestibulum
+        interdum ac neque et vulputate. Aenean pellentesque velit condimentum,
+        consequat nunc ut, semper est. Morbi non mi varius, hendrerit ex sit
+        amet, maximus justo. Aenean pretium ligula id dolor bibendum tincidunt.
+      </span>
+    </Accordion>
+    <Accordion initOpened summary={<span>Lorem Ipsum</span>}>
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+        commodo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+        nec lorem vitae metus gravida cursus vitae fringilla odio. Vestibulum
+        interdum ac neque et vulputate. Aenean pellentesque velit condimentum,
+        consequat nunc ut, semper est. Morbi non mi varius, hendrerit ex sit
+        amet, maximus justo. Aenean pretium ligula id dolor bibendum tincidunt.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec
+        lorem vitae metus gravida cursus vitae fringilla odio. Vestibulum
+        interdum ac neque et vulputate. Aenean pellentesque velit condimentum,
+        consequat nunc ut, semper est. Morbi non mi varius, hendrerit ex sit
+        amet, maximus justo. Aenean pretium ligula id dolor bibendum tincidunt.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec
+        lorem vitae metus gravida cursus vitae fringilla odio. Vestibulum
+        interdum ac neque et vulputate. Aenean pellentesque velit condimentum,
+        consequat nunc ut, semper est. Morbi non mi varius, hendrerit ex sit
+        amet, maximus justo. Aenean pretium ligula id dolor bibendum tincidunt.
+      </span>
+    </Accordion>
+  </Theme>
+);
