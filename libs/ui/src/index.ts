97beb87d8bcec5eae2a17aa6e94b5f154cef1c6a
Gustavo Bento de Paula <gustavob.paula@gmail.com>
2020-04-01 18:46:34 -0300

    feat: :rocket: export component

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,6 +1,7 @@
 export * from './lib/display/Text';
 export * from './lib/forms/TextField';
 export * from './lib/general/Theme';
+export * from './lib/navigation/Accordion';
 
 export { default as themeGuiaBolso } from './designSystem/tokens/guiaBolso';
 export { default as themeConnect } from './designSystem/tokens/connect';
