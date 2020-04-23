34b04ced1e484f90b5a817363785b620a3a1ea0a
Thaís Fukuda <fukudathais@gmail.com>
2020-04-23 12:07:28 -0300

    feat: export button component and reorder components alphabetically

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,9 +1,9 @@
+export * from './lib/forms/Button';
+export * from './lib/containers/Card';
 export * from './lib/display/Text';
 export * from './lib/forms/TextField';
 export * from './lib/general/Theme';
 export * from './lib/navigation/Accordion';
-export * from './lib/containers/Card';
-
 export * from './lib/navigation/Steps';
 
 export { default as themeGuiaBolso } from './designSystem/tokens/guiaBolso';
