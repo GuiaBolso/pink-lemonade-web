7a425fbaaf4f685f41fdc51a71af899c946fc36f
Thaís Fukuda <fukudathais@gmail.com>
2020-04-23 15:32:53 -0300

    refactor: emotion styled

--- libs/ui/src/lib/forms/Button/Button.style.ts
+++ libs/ui/src/lib/forms/Button/Button.style.ts
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/core';
 
 import MuiButton from '@material-ui/core/Button';
 
@@ -9,89 +10,82 @@ type ButtonProps = {
 };
 
 export const Button = styled(MuiButton)`
-  &.MuiButton {
-    &-root {
-      border-radius: 8px;
-      min-height: 48px;
-      padding: 0 24px;
+  ${({ theme }: ButtonProps) => {
+    return css`
+      &.MuiButton {
+        &-root {
+          border-radius: 8px;
+          min-height: 48px;
+          padding: 0 24px;
 
-      &:not(.MuiButton-fullWidth) {
-        width: 300px;
-      }
-    }
+          &:not(.MuiButton-fullWidth) {
+            width: 300px;
+          }
+        }
 
-    &-contained {
-      background-color: ${({ theme }: ButtonProps) =>
-        theme?.colors?.brand.secondary.default};
-      color: #fff;
+        &-contained {
+          background-color: ${theme?.colors?.brand.secondary.default};
+          color: #fff;
 
-      &:hover {
-        background-color: ${({ theme }: ButtonProps) =>
-          theme?.colors?.brand.secondary.light};
-      }
+          &:hover {
+            background-color: ${theme?.colors?.brand.secondary.light};
+          }
 
-      &:active {
-        background-color: ${({ theme }: ButtonProps) =>
-          theme?.colors?.brand.secondary.dark};
-      }
+          &:active {
+            background-color: ${theme?.colors?.brand.secondary.dark};
+          }
 
-      &.Mui-disabled {
-        background-color: ${({ theme }: ButtonProps) =>
-          theme?.colors?.neutral.brand};
-        color: ${({ theme }: ButtonProps) => theme?.colors?.neutral.blank};
-      }
-    }
+          &.Mui-disabled {
+            background-color: ${theme?.colors?.neutral.brand};
+            color: ${theme?.colors?.neutral.blank};
+          }
+        }
 
-    &-outlined,
-    &-text {
-      color: ${({ theme }: ButtonProps) =>
-        theme?.colors?.brand.secondary.default};
+        &-outlined,
+        &-text {
+          color: ${theme?.colors?.brand.secondary.default};
 
-      &:hover {
-        background-color: transparent;
-        color: ${({ theme }: ButtonProps) =>
-          theme?.colors?.brand.secondary.light};
-      }
+          &:hover {
+            background-color: transparent;
+            color: ${theme?.colors?.brand.secondary.light};
+          }
 
-      &:active {
-        color: ${({ theme }: ButtonProps) =>
-          theme?.colors?.brand.secondary.dark};
+          &:active {
+            color: ${theme?.colors?.brand.secondary.dark};
+          }
+
+          .MuiButton-label {
+            font-weight: bold;
+          }
+        }
+
+        &-outlined {
+          border-color: ${theme?.colors?.brand.secondary.default};
+
+          &:hover {
+            border-color: ${theme?.colors?.brand.secondary.light};
+            border-width: 2px;
+          }
+
+          &:active {
+            border-color: ${theme?.colors?.brand.secondary.dark};
+          }
+        }
+
+        &-sizeSmall {
+          min-height: 40px;
+        }
+
+        &-fullWidth {
+          border-radius: 0;
+        }
       }
 
       .MuiButton-label {
-        font-weight: bold;
+        font-family: ${theme?.type?.fontFamily?.neutral};
+        font-weight: 600;
+        text-transform: none;
       }
-    }
-
-    &-outlined {
-      border-color: ${({ theme }: ButtonProps) =>
-        theme?.colors?.brand.secondary.default};
-
-      &:hover {
-        border-color: ${({ theme }: ButtonProps) =>
-          theme?.colors?.brand.secondary.light};
-        border-width: 2px;
-      }
-
-      &:active {
-        border-color: ${({ theme }: ButtonProps) =>
-          theme?.colors?.brand.secondary.dark};
-      }
-    }
-
-    &-sizeSmall {
-      min-height: 40px;
-    }
-
-    &-fullWidth {
-      border-radius: 0;
-    }
-  }
-
-  .MuiButton-label {
-    font-family: ${({ theme }: ButtonProps) =>
-      theme?.type?.fontFamily?.neutral};
-    font-weight: 600;
-    text-transform: none;
-  }
+    `;
+  }};
 `;
