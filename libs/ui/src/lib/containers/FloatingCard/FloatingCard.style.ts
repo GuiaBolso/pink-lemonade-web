2d7efc3625a5bd9b165fb13651a461ad96c94beb
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2020-05-13 11:11:49 -0300

    feat: floating card component

---
+++ libs/ui/src/lib/containers/FloatingCard/FloatingCard.style.ts
@@ -0,0 +1,52 @@
+import styled from '@emotion/styled';
+
+// eslint-disable-next-line import/no-unresolved
+import { Text } from '@guiabolsobr/ui';
+
+// eslint-disable-next-line import/no-unresolved
+import { pxToRem } from '@guiabolsobr/utils';
+
+import { CustomThemeProps } from '../../../typings/CustomThemeProps';
+
+type FloatingCardTheme = {
+  theme: CustomThemeProps;
+};
+
+export const FloatingCard = styled.article`
+  background: #fff;
+  border-radius: ${pxToRem(4)};
+  flex: 1;
+  margin: 0 ${pxToRem(2)};
+  padding: ${pxToRem(16)} ${pxToRem(20)} ${pxToRem(24)} ${pxToRem(20)};
+  transition: box-shadow 0.2s, transform 0.3s;
+
+  &:hover {
+    box-shadow: 0px 8px 8px #d9d9d9;
+    transform: translateY(-5px);
+  }
+
+  &:empty {
+    display: none;
+  }
+
+  @media all and (max-width: 768.5px) {
+    & + article {
+      margin-top: ${pxToRem(10)};
+    }
+  }
+`;
+
+export const Category = styled(Text)`
+  color: ${({ theme }: FloatingCardTheme) =>
+    theme?.colors?.brand?.secondary?.default};
+  text-transform: uppercase;
+`;
+
+export const Title = styled(Text)`
+  margin: ${pxToRem(16)} 0;
+`;
+
+export const Description = styled(Text)`
+  color: ${({ theme }: FloatingCardTheme) => theme?.colors?.neutral?.darker};
+  margin: ${pxToRem(8)} 0;
+`;
---
+++ libs/ui/src/lib/containers/FloatingCard/FloatingCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import * as S from './FloatingCard.style';
+
+interface FloatingCardProps {
+  category: string;
+  title: string;
+  description: string;
+}
+
+const FloatingCard = ({ category, title, description }: FloatingCardProps) => (
+  <S.FloatingCard>
+    <S.Category variant="caption" component="h3">
+      {category}
+    </S.Category>
+
+    <S.Title variant="heading-04" component="h1">
+      {title}
+    </S.Title>
+
+    <S.Description variant="body-02" component="p">
+      {description}
+    </S.Description>
+  </S.FloatingCard>
+);
+
+export default FloatingCard;
