d184f856a92a8187a4d5a25863cec34e1df949cf
Gustavo Bento de Paula <gustavob.paula@gmail.com>
2020-05-28 12:39:37 -0300

    feat: :rocket: add util copy to clipboard

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -5,3 +5,5 @@ export * from './lib/hooks/use-did-update/use-did-update';
 export * from './lib/hooks/use-magic-state/use-magic-state';
 export * from './lib/object/merge-right/merge-right';
 export * from './lib/scales/px-to-rem/px-to-rem';
+
+export * from './lib/copy-to-clipboard/copy-to-clipboard';
