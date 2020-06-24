ea8ea8970424f621fb2217eba33a36ddf1510386
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2020-06-24 17:04:59 -0300

    feat: component infopanel

--- libs/ui/src/lib/notification/infoPanel/InfoPanel.tsx
+++ libs/ui/src/lib/notification/infoPanel/InfoPanel.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
+import warningIcon from '../../../assets/images/icons/warning-blue.svg';
+
 import * as S from './InfoPanel.style';
 
-const InfoPanel: React.FC = () => {
-  return (
-    <S.Container>
-      <h1>InfoPanel</h1>
-    </S.Container>
-  );
+type infoPanelProps = {
+  imagePath?: string;
+  children: React.ReactNode;
 };
+
+const InfoPanel = ({ imagePath = warningIcon, children }: infoPanelProps) => (
+  <S.Container>
+    <S.Image src={imagePath} alt="aviso" />
+    <S.Content>{children}</S.Content>
+  </S.Container>
+);
 
 export default InfoPanel;
