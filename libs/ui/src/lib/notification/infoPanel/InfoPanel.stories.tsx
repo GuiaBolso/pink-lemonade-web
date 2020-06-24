f362caaa5fcccca157698d238357f7c7fcc884dc
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2020-06-24 10:15:39 -0300

    feat: initial component structure

---
+++ libs/ui/src/lib/notification/infoPanel/InfoPanel.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { InfoPanel } from './index';
+
+export default {
+  title: 'Notification/InfoPanel',
+  component: InfoPanel,
+};
+
+export const Default = () => <InfoPanel />;
---
+++ libs/ui/src/lib/notification/infoPanel/InfoPanel.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './InfoPanel.style';
+
+const InfoPanel: React.FC = () => {
+  return (
+    <S.Container>
+      <h1>InfoPanel</h1>
+    </S.Container>
+  );
+};
+
+export default InfoPanel;
