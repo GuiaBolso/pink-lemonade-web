527402a8ded75e4f2e10f0072dcb62b7bf8fe2ba
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2020-07-28 17:52:54 -0300

    feat: 🎸 componente usage

--- libs/ui/src/lib/navigation/DropMenu/DropMenu.stories.tsx
+++ libs/ui/src/lib/navigation/DropMenu/DropMenu.stories.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 
-import { DropMenu } from './index';
+import { DropMenu, DropMenuItem } from './index';
 
 export default {
   title: 'Navigation/DropMenu',
   component: DropMenu,
 };
 
-export const ComponentDropMenu = () => (
-  <div
-    style={{
-      maxWidth: '300px',
-      position: 'relative',
-    }}
-  >
-    <DropMenu />
-  </div>
-);
+export const ComponentDropMenu = () => {
+  return (
+    <div
+      style={{
+        maxWidth: '300px',
+        padding: '1em',
+        position: 'relative',
+      }}
+    >
+      <DropMenu executeOnClose={() => alert('[MENU] executeOnClose funcion')}>
+        <DropMenuItem onClick={() => alert('[ITEM] clicked')}>
+          Click to execute a funcion
+        </DropMenuItem>
+        <DropMenuItem>Some item</DropMenuItem>
+        <DropMenuItem>Some item a little bigger</DropMenuItem>
+        <DropMenuItem>Another item</DropMenuItem>
+        <DropMenuItem>One more item</DropMenuItem>
+      </DropMenu>
+    </div>
+  );
+};
