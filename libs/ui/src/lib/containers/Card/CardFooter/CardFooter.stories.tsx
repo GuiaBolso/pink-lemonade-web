205235b1c025834315710a28169910656abc81d2
Gustavo Bento de Paula <gustavob.paula@gmail.com>
2020-09-11 18:25:47 -0300

    feat: 🎸 update CardFooter storybook

--- libs/ui/src/lib/containers/Card/CardFooter/CardFooter.stories.tsx
+++ libs/ui/src/lib/containers/Card/CardFooter/CardFooter.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { StoryFn } from '@storybook/addons';
+import { Story, Meta } from '@storybook/react/types-6-0';
 
-import { CardFooter } from '../index';
+import CardFooter, { CardFooterProps } from './CardFooter';
 import { Action } from '../../../forms/Action';
 import { Button } from '../../../forms/Button';
 
@@ -19,20 +20,23 @@ export default {
       </div>
     ),
   ],
+} as Meta;
+
+const Template: Story<CardFooterProps> = args => <CardFooter {...args} />;
+
+export const addendum = Template.bind({});
+
+addendum.args = {
+  addendum: true,
+  description:
+    'Você utiliza outro banco? Aproveite para adicionar enquanto carrega a conta acima',
+  action: (
+    <Action scale="narrow" label="Action label" arrowPosition="trailing" />
+  ),
 };
 
 export const action = () => (
   <CardFooter
-    action={
-      <Action scale="narrow" label="Action label" arrowPosition="trailing" />
-    }
-  />
-);
-
-export const addendum = () => (
-  <CardFooter
-    addendum
-    description="Você utiliza outro banco? Aproveite para adicionar enquanto carrega a conta acima"
     action={
       <Action scale="narrow" label="Action label" arrowPosition="trailing" />
     }
