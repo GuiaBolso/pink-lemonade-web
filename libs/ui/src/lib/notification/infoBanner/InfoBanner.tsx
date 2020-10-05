1497a32e24544f4244dda7e917c35c039799b5d0
Cazuza <cazuzaneves@gmail.com>
2020-10-05 07:16:36 -0300

    feat: handle confirm

--- libs/ui/src/lib/notification/infoBanner/InfoBanner.tsx
+++ libs/ui/src/lib/notification/infoBanner/InfoBanner.tsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import * as S from './InfoBanner.style';
 
 type InfoBannerProps = {
   children: React.ReactNode;
   type: 'base' | 'text-button' | 'text-close';
+  confirm?: {
+    label: string;
+    handler?: () => void;
+  };
 };
 
-const InfoBanner = ({ children, type }: InfoBannerProps) => (
-  <S.Container>
-    <S.Content>
-      {children}
-      {type !== 'text-button' && <S.Close />}
-      {type !== 'text-close' && (
-        <S.BannerButton label="Button" size="small" scale="narrow" />
-      )}
-    </S.Content>
-  </S.Container>
-);
+const InfoBanner = ({ children, type, confirm }: InfoBannerProps) => {
+  const handleConfirm = useCallback(() => {
+    confirm?.handler?.();
+  }, []);
+
+  return (
+    <S.Container>
+      <S.Content>
+        {children}
+        {type !== 'text-button' && <S.Close />}
+        {type !== 'text-close' && (
+          <S.BannerButton
+            label={confirm?.label || 'Concordo'}
+            onClick={handleConfirm}
+            size="small"
+            scale="narrow"
+          />
+        )}
+      </S.Content>
+    </S.Container>
+  );
+};
 
 export default InfoBanner;
