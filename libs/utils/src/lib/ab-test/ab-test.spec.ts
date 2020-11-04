6a3237997f7fda6cd598c52f1a233678bcafe19b
Gustavo Bento de Paula <gustavob.paula@gmail.com>
2020-11-04 16:50:57 -0300

    feat: 🎸 add sessionStorage in ab test helper

--- libs/utils/src/lib/ab-test/ab-test.spec.ts
+++ libs/utils/src/lib/ab-test/ab-test.spec.ts
@@ -3,9 +3,11 @@ import { setABTest, getABTestValue, getAllABTestValues } from './ab-test';
 describe('A/B Test', () => {
   setABTest('test1', 'first test');
   setABTest('test2', 'second test');
+  setABTest('test3', 'third test', 'sessionStorage');
 
   const test1 = getABTestValue('test1');
   const test2 = getABTestValue('ab-test-test2');
+  const test3 = getABTestValue('test3');
 
   it('Should set a new item', () => {
     expect(test1).toBe('first test');
@@ -15,9 +17,13 @@ describe('A/B Test', () => {
     expect(test2).toBe('second test');
   });
 
+  it('Should get in session storage', () => {
+    expect(test3).toBe('third test');
+  });
+
   it('Should get all in a array of object', () => {
     const tests = getAllABTestValues();
 
-    expect(tests).toHaveLength(2);
+    expect(tests).toHaveLength(3);
   });
 });
