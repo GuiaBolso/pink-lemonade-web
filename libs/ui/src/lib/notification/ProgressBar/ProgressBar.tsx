0937480a29085f1ad73b79de28722d9d85f67aa2
cazuzabarberino <cazuzaneves@gmail.com>
2021-02-11 00:41:49 -0300

    feat: new props

--- libs/ui/src/lib/notification/ProgressBar/ProgressBar.tsx
+++ libs/ui/src/lib/notification/ProgressBar/ProgressBar.tsx
@@ -2,6 +2,7 @@ import React, { memo } from 'react';
 import { Text } from '@guiabolsobr/ui';
 
 import check from '../../../assets/images/icons/check.svg';
+import error from '../../../assets/images/icons/alert.svg';
 
 import * as S from './ProgressBar.style';
 
@@ -9,6 +10,8 @@ export type ProgressBarProps = {
   variant?: 'determinate' | 'indeterminate';
   value?: number;
   label: string;
+  hasError?: boolean;
+  customIcon?: React.ReactNode;
   color?: {
     line: string;
     subline: string;
@@ -19,11 +22,14 @@ export const ProgressBar = ({
   variant = 'indeterminate',
   value,
   label,
+  hasError,
+  customIcon,
   color,
 }: ProgressBarProps) => {
   if ((value && value < 0) || value > 100) {
     throw new Error('The value field must be an integer between 0 and 100');
   }
+
   return (
     <S.Container>
       <S.Line lineColor={color?.line}>
@@ -39,11 +45,16 @@ export const ProgressBar = ({
       </S.Line>
       <S.Info>
         <Text variant="subtitle-small">{label}</Text>
-        {value?.toString &&
-          (value === 100 ? (
-            <S.CheckIcon src={check} alt="sucesso" />
+        {customIcon ||
+          (hasError ? (
+            <S.IconImg src={error} alt="error" />
           ) : (
-            <Text variant="subtitle-small">{value}%</Text>
+            value?.toString &&
+            (value === 100 ? (
+              <S.IconImg src={check} alt="sucesso" />
+            ) : (
+              <Text variant="subtitle-small">{value}%</Text>
+            ))
           ))}
       </S.Info>
     </S.Container>
