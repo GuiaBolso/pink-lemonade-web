ccce84d7f0485baa67702ede3491fa7abf226a13
Henrique Moreira <henryqrm@gmail.com>
2021-05-25 09:56:45 -0300

    fix: 🐛 class -> className

--- libs/ui/src/lib/icons/ExpandMore.tsx
+++ libs/ui/src/lib/icons/ExpandMore.tsx
@@ -4,7 +4,7 @@ import { IconSVG } from './icons.style';
 
 export const ExpandMore = props => (
   <IconSVG
-    class="MuiSvgIcon-root"
+    className="MuiSvgIcon-root"
     focusable="false"
     viewBox="0 0 24 24"
     aria-hidden="true"
