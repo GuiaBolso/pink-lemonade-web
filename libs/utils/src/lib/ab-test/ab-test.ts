6150b56970e6e8fc94a40d455a242d5c0cd21c90
Henrique Moreira <henryqrm@gmail.com>
2021-05-31 15:31:20 -0300

    fix: 🐛 add support storage

--- libs/utils/src/lib/ab-test/ab-test.ts
+++ libs/utils/src/lib/ab-test/ab-test.ts
@@ -1,52 +1,37 @@
+import { local, session } from '../storage';
+const sessionKeys = [];
+const localKeys = [];
+
 export const setABTest = (
   name: string,
   value: string,
   storage: 'localStorage' | 'sessionStorage' = 'localStorage',
 ) => {
-  if (!window || !window.localStorage || !window.sessionStorage) return;
-
   if (storage === 'sessionStorage') {
-    return sessionStorage.setItem(`ab-test-${name}`, value);
+    const key = `ab-test-${name}`;
+    sessionKeys.push(key);
+
+    return session.setItem(key, value);
   }
 
-  return localStorage.setItem(`ab-test-${name}`, value);
+  const key = `ab-test-${name}`;
+  localKeys.push(key);
+
+  return local.setItem(key, value);
 };
 
 export const getABTestValue = (name: string) => {
-  if (!window || !window.localStorage || !window.sessionStorage) return;
-
   return (
-    localStorage.getItem(`ab-test-${name}`) ||
-    localStorage.getItem(name) ||
-    sessionStorage.getItem(`ab-test-${name}`) ||
-    sessionStorage.getItem(name)
+    local.getItem(`ab-test-${name}`) ||
+    local.getItem(name) ||
+    session.getItem(`ab-test-${name}`) ||
+    session.getItem(name)
   );
 };
 
 export const getAllABTestValues = ():
   | []
-  | { name: string; value: string }[] => {
-  if (!window || !window.localStorage || !window.sessionStorage) return;
-
-  const tests = [];
-
-  for (const key in localStorage) {
-    if (key.indexOf('ab-test') !== -1) {
-      tests.push({
-        name: key,
-        value: localStorage.getItem(key),
-      });
-    }
-  }
-
-  for (const key in sessionStorage) {
-    if (key.indexOf('ab-test') !== -1) {
-      tests.push({
-        name: key,
-        value: sessionStorage.getItem(key),
-      });
-    }
-  }
-
-  return tests;
-};
+  | { name: string; value: string }[] => [
+  ...sessionKeys.map(session.getItem),
+  ...localKeys.map(local.getItem),
+];
