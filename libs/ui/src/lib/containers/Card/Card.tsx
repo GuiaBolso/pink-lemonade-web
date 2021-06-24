192c214c2d975661a9e15552d784741826b3744e
Gustavo Bento de Paula <gustavob.paula@gmail.com>
2021-06-24 16:41:09 -0300

    fix: 🐛 pass props to card

--- libs/ui/src/lib/containers/Card/Card.tsx
+++ libs/ui/src/lib/containers/Card/Card.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 import * as S from './Card.style';
 
-export type CardProps = {
-  children?: React.ReactNode;
+export type CardProps = React.ComponentProps<'article'> & {
   variant?: 'default' | 'outlined';
 };
 
-const Card = ({ children, variant = 'default' }: CardProps) => {
-  return <S.Card variant={variant}>{children}</S.Card>;
+const Card = ({ children, variant = 'default', ...rest }: CardProps) => {
+  return (
+    <S.Card variant={variant} {...rest}>
+      {children}
+    </S.Card>
+  );
 };
 
 export default Card;
